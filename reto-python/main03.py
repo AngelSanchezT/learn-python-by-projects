b39c67f1035b7174c94548d0496571c2cf685183
Angel Javier Sanchez <ajsanchez22@gmail.com>
2024-02-10 21:49:36 -0500

    #RetoPython: ✅ Dia 3: Miércoles

--- reto-python/main03.py
+++ reto-python/main03.py
@@ -5,12 +5,16 @@ Vaya, ya llegamos al reto número 3 de la semana, y para este tercer reto lo que
 
 Estas funcionalidades son las siguientes
 
-1.- Siempre que se registre un nuevo usuario de forma exitosa generaremos un identificador único para este registro/usuario. Te recomiendo sea un ID numérico auto incremental, que comience en 1 hasta N. Sin embargo siéntete libre elegir el formato o tipo que tú desees.
-2.- Todos estos nuevos identificadores deberán almacenarse en un listado que será impreso en consola cuando todos los registros se hayan creado. Esto de tal forma que el usuario pueda conocer y tenga certeza que las operaciones, en efecto, se realizaron de forma exitosa.
+1.- Siempre que se registre un nuevo usuario de forma exitosa generaremos un identificador único para este registro/usuario. 
+Te recomiendo sea un ID numérico auto incremental, que comience en 1 hasta N. 
+Sin embargo siéntete libre elegir el formato o tipo que tú desees.
+
+2.- Todos estos nuevos identificadores deberán almacenarse en un listado que será impreso en consola cuando todos 
+los registros se hayan creado. Esto de tal forma que el usuario pueda conocer y tenga certeza que las operaciones, 
+en efecto, se realizaron de forma exitosa.
 
 Con estas 2 nuevas funcionalidades es probable te intuyas como iremos finalizando nuestro programa para el quinto día.
 
-Entrega tu ejercicio aquí
 """
 
 import os
